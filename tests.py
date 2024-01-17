ca5c1fc3b71cbb8c1b62fe3a7d064de686f26472
Reece-Ausmus <112425322+Reece-Ausmus@users.noreply.github.com>
2024-01-16 21:49:50 -0500

    suppressed print statements when running test script

--- tests.py
+++ tests.py
@@ -1,31 +1,37 @@
 import unittest
 import os
 from huffman_compression import compress, decompress
+from unittest.mock import patch
+import io
 
 class TestHuffmanCompression(unittest.TestCase):
 
     def test_compress_and_decompress(self):
-        input_file = 'Test_Files/txt_example.txt'
-        output_file = 'Test_Files/txt_example_output.bin'
+        input_file = 'Test_Files/html_example.html'
+        compressed_file = 'Test_Files/html_example_output.bin'
+        decompressed_file = 'Test_Files/html_example_decompressed.html'
 
-        # Compress the file
-        compress(input_file, output_file)
+        # Suppress print statements in compress and decompress methods
+        with patch('sys.stdout', new_callable=io.StringIO):
+            # Compress the file
+            compress(input_file, compressed_file)
 
-        # Decompress the file
-        decompress(output_file, 'Test_Files/txt_example_decompressed.txt')
+            # Decompress the file
+            decompress(compressed_file, decompressed_file)
 
         # Check if the decompressed file content is the same as the original input
-        with open('Test_Files/txt_example_decompressed.txt', 'rb') as file:
+        with open(decompressed_file, 'rb') as file:
             decompressed_data = file.read()
 
         with open(input_file, 'rb') as file:
             original_data = file.read()
 
         self.assertEqual(decompressed_data, original_data)
+        self.assertTrue(os.path.getsize(compressed_file) <= os.path.getsize(input_file))
 
         # Clean up test files
-        os.remove(output_file)
-        os.remove('Test_Files/txt_example_decompressed.txt')
+        os.remove(compressed_file)
+        os.remove(decompressed_file)
 
 if __name__ == '__main__':
     unittest.main()
